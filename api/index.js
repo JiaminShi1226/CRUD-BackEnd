2e690789c050734ab32d6b7e13ff24cd802ae223
JiaminShi1226 <shijiamin0324@gmail.com>
2023-06-28 16:57:51 -0400

    getAllCampuses route added

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 
 // Mounted on /api/
-router.use("/campuses" , require("./campuses"));
-router.use("/students", require("./students"));
+router.use("/campuses", require("./campus/getAllCampuses"));
+router.use("/students", require("./student/getAllStudents"));
 
 router.use((req, res, next) => {
-   const error = new Error("404 Not Found");
-   error.status = 404;
-   next(error);
+  const error = new Error("404 Not Found");
+  error.status = 404;
+  next(error);
 });
 
 module.exports = router;
