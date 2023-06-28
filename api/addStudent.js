735e1eb92b66566e78590999e6c492decf93356e
Tashi <tashishyangba23@gmail.com>
2023-06-28 16:45:57 -0400

    add student to database

---
+++ api/addStudent.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const {Student} = require("../db/models");
+
+router.post("/", async(req, res, next)=>{
+    const {firstName, lastName, email,imageUrl, gpa } = req.body;
+    console.log("Add student hit");
+    try {
+      const newStudent = await Student.create({firstName, lastName, email,imageUrl, gpa});
+      res.send(newStudent);
+    } catch (error) {
+        next(error);
+    }
+})
+
+module.exports = router;
