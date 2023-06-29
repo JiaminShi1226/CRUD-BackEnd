cacdb688cc2801f58a9167ca2001894535976b74
Tashi <tashishyangba23@gmail.com>
2023-06-29 11:17:17 -0400

    remove student by ID

--- api/student/getStudentByID.js
+++ api/student/getStudentByID.js
@@ -3,13 +3,13 @@ const router = express.Router();
 const { Student } = require("../../db/models");
 
 //finds and returns the student with the id
-//http://localhost:8080/api/students/getStudentByID/3
+//http://localhost:8080/api/students/getStudentByID/:id
 router.get("/:id", async (req, res, next) => {
   try {
     const student = await Student.findByPk(req.params.id);
     student
       ? res.status(200).json(student)
-      : res.status(404).send("Campus Not Found");
+      : res.status(404).send("Student Not Found");
   } catch (error) {
     next(error);
   }
