d3f0aff2f9ecd6859509f385841554c90305e73c
Tashi <tashishyangba23@gmail.com>
2023-06-29 08:29:40 -0400

    added a ternary operator to addStudent

--- api/student/getAllStudents.js
+++ api/student/getAllStudents.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const {Student} = require("../../db/models");
 //Send a response will all the student from database when requested
 router.get("/",async(req,res,next)=>{
+    console.log("Get all student hit");
     try {
         const allStudents = await Student.findAll();
         allStudents
