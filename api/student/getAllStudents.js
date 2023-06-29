cdd68cc1e073691d0994d20139e20a553a28a424
Tashi <tashishyangba23@gmail.com>
2023-06-29 08:13:25 -0400

    added allStudents Route

---
+++ api/student/getAllStudents.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+const {Student} = require("../../db/models");
+//Send a response will all the student from database when requested
+router.get("/",async(req,res,next)=>{
+    try {
+        const allStudents = await Student.findAll();
+        allStudents
+        ?res.status(202).json(allStudents) //if allStudent is truthy
+        :res.status(404).send("Student List Not Found"); //if allStudent is falsey
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = router;
