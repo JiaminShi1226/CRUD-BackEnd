ac3557c4c405b62a963425b26acfd4dfc954bf03
JiaminShi1226 <shijiamin0324@gmail.com>
2023-06-29 08:44:08 -0400

    fixed typo, add seed.js, fixed router problem

--- api/student/index.js
+++ api/student/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 // Mounted on /api/student/
-router.use("/getAllStudents", require("./getAllStudents"));
+
 router.use("/addStudent", require("./addStudent"));
 
 router.use((req, res, next) => {
