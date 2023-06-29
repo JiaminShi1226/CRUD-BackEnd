a90fbed11383caa7705711204dda25bc744ae3cd
Tashi <tashishyangba23@gmail.com>
2023-06-29 11:02:18 -0400

    find student by id

--- api/student/index.js
+++ api/student/index.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 
 // Mounted on /api/student/
-
 router.use("/addStudent", require("./addStudent"));
 router.use("/getAllStudents", require("./getAllStudents"));
+router.use("/getStudentByID", require("./getStudentByID"));
+
 router.use((req, res, next) => {
   const error = new Error("404 Not Found");
   error.status = 404;
