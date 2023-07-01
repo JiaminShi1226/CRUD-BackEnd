4d279fc362a4b022b92912d1e5943d7334f88d34
Tashi <tashishyangba23@gmail.com>
2023-07-01 12:09:02 -0400

    added working dotenv

--- db/db.js
+++ db/db.js
@@ -6,20 +6,10 @@ const dotenv = require("dotenv");
 dotenv.config();
 // name === crudbackend
 
-//change to your own username and password
-// const db = new Sequelize(`postgres://postgres:0917@localhost:5432/${name}`, {
-//   logging: false,
-// });
-
+//Database
 const db = new Sequelize(`${process.env.REACT_APP_DATABASE}`, {
   logging: false,
 });
-
-console.log(process.env.REACT_APP_TEST);
-
-// const db = new Sequelize(`postgres://postgres:TashiShyangba23@localhost:5432/${name}`, {
-//   logging: false,
-// });
 
 
 
