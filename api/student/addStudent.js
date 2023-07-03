6e6fdf5c075e96b11a1f914c3d56bf8dd94f8673
Tashi <tashishyangba23@gmail.com>
2023-07-03 14:28:52 -0400

    edited a spelling mistake

--- api/student/addStudent.js
+++ api/student/addStudent.js
@@ -4,9 +4,8 @@ const {Student} = require("../../db/models");
 //http://localhost:8080/api/students/addStudent
 router.post("/", async(req, res, next)=>{
     const {firstName, lastName, email,imageUrl, gpa, campusId } = req.body;
-    console.log("Add student hit");
     try {
-      const newStudent = await Student.create({firstName, lastName, email,imageUrl, gpa, campusId});
+      const newStudent = await Student.create({firstName, lastName, email, imageUrl, gpa, campusId});
       newStudent
       ?res.status(200).json(newStudent) //if allStudent is truthy
       :res.status(404).send("Unsuccessful In Adding Student"); //if allStudent is falsey
